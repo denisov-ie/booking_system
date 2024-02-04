e95d5003d44eef49f59b2400dfadc6c5ea3ba42a
denisov-ie <denisov.ilia@gmail.com>
2024-02-04 05:30:40 +0300

    spring security, web, thymeleaf

--- src/main/resources/db/default_authorities.sql
+++ src/main/resources/db/default_authorities.sql
@@ -1,7 +1,9 @@
 -- liquibase formatted sql
 
--- changeset ie-denisov:users
+-- changeset ie-denisov:roles_users
 INSERT INTO public.roles(role_id, role) VALUES (0, 'ADMIN');
+INSERT INTO public.roles(role_id, role) VALUES (1, 'USER');
+INSERT INTO public.roles(role_id, role) VALUES (2, 'SUPPORT');
 
 -- Hint: login = ADMIN, password = test
 INSERT INTO public.users(user_id, login, pass, role_id) VALUES (0, 'ADMIN', '{bcrypt}$2a$10$LIe2vrJgjwLaIwFtuSNvquXzQF9Zc2BoLRSeWsmUc/.fga0n489Bm', 0);
